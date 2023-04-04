fe3a004e338eafd25d66dee7fa6ff756126c38c1
eitamkatz <eitam.katz@mail.huji.ac.il>
2023-04-04 13:14:10 +0300

    eitam - change the movement to constant speed

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -6,16 +6,36 @@ using Unity.Collections;
 using UnityEngine;
 using UnityEngine.Animations;
 using UnityEngine.PlayerLoop;
+using Quaternion = UnityEngine.Quaternion;
 using Vector2 = UnityEngine.Vector2;
+using Vector3 = UnityEngine.Vector3;
 
 public class Player : MonoBehaviour
 {
     private Vector2 direction = Vector2.zero;
+    // private bool flag = true;
+    private float timer;
+    [SerializeField] float speed = 0.05f;
+    // [SerializeField] private float speed = 1f;
 
-    void Update()
+    private void Start()
+    {
+        timer = Time.time;
+    }
+
+    private void Update()
     {
         CheckInput();
+        RotationPlayer();
     }
+
+    private void FixedUpdate()
+    {
+
+        UpdateDirection();
+        // StartCoroutine(UpdateDirection());
+    }
+
 
     private void CheckInput()
     {
@@ -28,8 +48,31 @@ public class Player : MonoBehaviour
             direction = Vector2.up;
         else if (Input.GetKeyDown(KeyCode.DownArrow))
             direction = Vector2.down;
-        else 
-            direction = Vector2.zero;
-        transform.Translate(direction);
+        // else 
+        //     direction = Vector2.zero;
     }
+
+    private void UpdateDirection()
+    {
+        if (Time.time - timer > speed)
+        {
+            Vector3 position = new Vector3(
+                Mathf.Round(transform.position.x) + direction.x,
+                Mathf.Round(transform.position.y) + direction.y, 0.0f);
+            transform.position = position;
+            timer = Time.time;
+        }
+    }
+
+    // private void RotationPlayer()
+    // {
+    //     if (Input.GetKeyDown(KeyCode.Space))
+    //     {
+    //         print("hi");
+    //         float newZ = transform.rotation.z - 90f;
+    //             
+    //         transform.rotation = Quaternion.Euler(0f, 0f, newZ);
+    //     }
+    // }
+    
 }
